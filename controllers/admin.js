5ba3d965adce488122d9488f70e34480e026b61f
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-09 13:24:21 +0530

    form for entering info about hackathon

--- controllers/admin.js
+++ controllers/admin.js
@@ -49,24 +49,65 @@ class adminController extends Telegram.TelegramBaseController {
     }
 
     enterHandler($){
-        let info=$.message.text.split(' ').slice(1).join(' ');
-
-        if(!info){
-            return $.sendMessage('Sorry please pass a info.')
+        let index=parseInt($.message.text.split(' ').slice(1)[0]);
+        
+        if(isNaN(index)){
+            return $.message('Sorry You did not entered a valid input');
         }
-        //using user session storage to store the hackathon info
-        $.getUserSession('hackathon'+`${index}`)
-        .then(info=>{
-            //if no hackathons yet
-            if(!Array.isArray(hackathons)) $.setUserSession('hackathons',[hackathon]);
-            
-            else $.setUserSession('hackathons',hackathons.concat([hackathon]));
-            
-            console.log(hackathon);
-            $.sendMessage('Added new hackathon!')
-         })
+        const form = {
+            date: {
+                q: 'Enter event date',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+                    if(message.text) {
+                        callback(true, message.text) //you must pass the result also
+                        return
+                    }
+        
+                    callback(false)
+                }
+            },
+            venue: {
+                q: 'Enter venue',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+                    if(message.text) {
+                        callback(true, message.text)
+                        return
+                    }
+        
+                    callback(false)
+                }
+            },
+            schedule: {
+                q: 'Enter event schedule',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+                    if(message.text) {
+                        callback(true, message.text)
+                        return
+                    }
+        
+                    callback(false)
+                }
+            }
+        }
+        
+        $.runForm(form, (result) => {
+            console.log(result)
+            $.getUserSession('hackathoninfo'+`${index}`)
+            .then(hackathoninfo=>{
+                //if no hackathon info yet
+                if(!Array.isArray(hackathoninfo)) $.setUserSession('hackathoninfo',[hackathoninfo]);
+                
+                else $.setUserSession('hackathoninfo',hackathoninfo.concat([result]));
+                
+                $.sendMessage('Added hackathon info!')
+             })
+        })
+        }
 
-    }
+    
 
     get routes() {
         return {

5226bc79f500d60bbd6c1400a532c8ad378106e8
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-09 11:28:01 +0530

    admin section

---
+++ controllers/otherwise.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Telegram=require('telegram-node-bot');
+
+class OtherwiseController extends Telegram.TelegramBaseController {
+    handle($){
+        $.sendMessage('Sorry I dont understand');
+    }   
+}
+
+module.exports=OtherwiseController;
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Telegram= require('telegram-node-bot');
+// PersistentMemoryStorage=require('./adapters/PersistentMemoryStorage'),
+// storage=new PersistentMemoryStorage(
+//     `${__dirname}/data/userStorage.json`,
+//     `${__dirname}/data/chatStorage.json`
+// );
+
+const tg=new Telegram.Telegram('571321316:AAESH5MDtz2f1fst0vel1SUecPSZRPfgg-k',{
+    workers:1
+    //storage:storage
+})
+
+// const PingController=require('./controllers/ping');
+const OtherwiseController=require('./controllers/otherwise');
+
+// tg.router.when(new Telegram.TextCommand('/ping','pingCommand'),new PingController())
+//     .otherwise(new OtherwiseController())
+
+const JudgeController=require('./controllers/judge');
+const AdminController=require('./controllers/admin');
+const ParticipantController=require('./controllers/participant');
+
+const judgeCtrl=new JudgeController();
+const adminCtrl=new AdminController();
+const participantCtrl=new ParticipantController();
+
+tg.router.when(new Telegram.TextCommand('/add','addCommand'),adminCtrl)
+.when(new Telegram.TextCommand('/get','getCommand'),adminCtrl)
+.when(new Telegram.TextCommand('/check','checkCommand'),adminCtrl)
+    .otherwise(new OtherwiseController())    
+
+// function exitHandler(exitCode){
+//     storage.flush();
+//     process.exit(exitCode);
+// }    
+
+// process.on('SIGINT',exitHandler.bind(null,0));
+// process.on('uncaughtException',exitHandler.bind(null,1));
